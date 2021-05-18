72be47782a837af7795e455ec700d82d2e5d8c26
findxc <liuxiaocui1993@gmail.com>
2021-05-18 22:40:55 +0800

    增加角色列表

--- src/pages/role-manage/index.jsx
+++ src/pages/role-manage/index.jsx
@@ -1,5 +1,62 @@
+import { useState, useEffect } from 'react'
+import { Table } from 'antd'
+import { getRoles } from './service'
+
 function RoleList() {
-  return <div>角色列表页</div>
+  const [loading, setLoading] = useState(false)
+  const [list, setList] = useState([])
+  const [total, setTotal] = useState(0)
+  const [filter, setFilter] = useState({ current: 1, pageSize: 10 })
+
+  const getList = (newFilter) => {
+    setFilter(newFilter)
+    setLoading(true)
+    getRoles(newFilter)
+      .then((res) => {
+        const { list, total } = res.data
+        setList(list)
+        setTotal(total)
+        setLoading(false)
+      })
+      .catch(() => {
+        setLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    getList(filter)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const onTableChange = (pagination) => {
+    const { current, pageSize } = pagination
+    getList({ current, pageSize })
+  }
+
+  const columns = [
+    { title: '角色名称', dataIndex: 'name', width: '20%' },
+    { title: '角色描述', dataIndex: 'desc' },
+  ]
+
+  const pagination = {
+    ...filter,
+    total,
+    showSizeChanger: true,
+    showTotal: (total) => `共 ${total} 条`,
+  }
+
+  return (
+    <div>
+      <Table
+        rowKey='id'
+        loading={loading}
+        columns={columns}
+        dataSource={list}
+        pagination={pagination}
+        onChange={onTableChange}
+      />
+    </div>
+  )
 }
 
 export default RoleList
