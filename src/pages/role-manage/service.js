a9343efb281c04832f2def21a72f5ff17af91b88
findxc <liuxiaocui1993@gmail.com>
2021-05-19 15:33:31 +0800

    增加添加/编辑角色的弹窗

--- src/pages/role-manage/service.js
+++ src/pages/role-manage/service.js
@@ -4,3 +4,11 @@ import { stringify } from 'qs'
 export function getRoles(filter) {
   return axios.get(`/api/roles?${stringify(filter)}`)
 }
+
+export function addRole(body) {
+  return axios.post('/api/role', body)
+}
+
+export function updateRole(id, body) {
+  return axios.put(`/api/role/${id}`, body)
+}
