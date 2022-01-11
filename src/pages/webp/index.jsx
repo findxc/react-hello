d4075b24da7a4e7e36b44549b6bb8456865d75b2
findxc <liuxiaocui1993@gmail.com>
2022-01-11 22:27:07 +0800

    增加自动生成.webp的脚本

--- src/pages/webp/index.jsx
+++ src/pages/webp/index.jsx
@@ -2,6 +2,7 @@ import { Divider } from 'antd'
 import img1 from './images/1.gif'
 import img2 from './images/2.png'
 import img3 from './images/3.png'
+import img4 from './images/4.jpeg'
 
 function Webp() {
   return (
@@ -9,10 +10,12 @@ function Webp() {
       <img alt='demo' src={img1} />
       <img alt='demo' src={img2} />
       <img alt='demo' src={img3} />
+      <img alt='demo' src={img4} />
       <Divider />
       <img alt='demo' src='/images/1.gif' />
       <img alt='demo' src='/images/2.png' />
       <img alt='demo' src='/images/3.png' />
+      <img alt='demo' src='/images/4.jpeg' />
     </div>
   )
 }
